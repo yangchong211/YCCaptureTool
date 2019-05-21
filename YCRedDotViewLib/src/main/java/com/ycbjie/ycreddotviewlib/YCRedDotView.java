93af7082f57083e0dfd4c299c25ae03d8bb2c609
yangchong211 <yangchong211@163.com>
2019-05-21 16:34:20 +0800

    封装红点控件

--- YCRedDotViewLib/src/main/java/com/ycbjie/ycreddotviewlib/YCRedDotView.java
+++ YCRedDotViewLib/src/main/java/com/ycbjie/ycreddotviewlib/YCRedDotView.java
@@ -1,3 +1,18 @@
+/*
+Copyright 2017 yangchong211（github.com/yangchong211）
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.ycbjie.ycreddotviewlib;
 
 import android.content.Context;
@@ -6,7 +21,6 @@ import android.graphics.Typeface;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RoundRectShape;
 import android.os.Build;
-import android.support.annotation.ColorRes;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v7.widget.AppCompatTextView;
@@ -17,17 +31,17 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.widget.FrameLayout.LayoutParams;
 import android.widget.TabWidget;
 
 
 public class YCRedDotView extends AppCompatTextView {
 
     /**
-     * 是否隐藏
+     * 是否隐藏红点上的数字
      */
     private boolean mHideOnNull = true;
+    private static final String ZERO = "0";
 
     public YCRedDotView(Context context) {
         this(context, null);
@@ -42,7 +56,6 @@ public class YCRedDotView extends AppCompatTextView {
         initView();
     }
 
-
     /**
      * 重新父类setText方法
      * @param text                  text
@@ -50,14 +63,17 @@ public class YCRedDotView extends AppCompatTextView {
      */
     @Override
     public void setText(CharSequence text, BufferType type) {
-        if (isHideOnNull() && (text == null || text.toString().equalsIgnoreCase("0"))) {
-            setVisibility(View.GONE);
+        if (isHideOnNull()) {
+            if (text == null || text.toString().equalsIgnoreCase(ZERO)){
+                setVisibility(View.GONE);
+            }else {
+                setVisibility(View.VISIBLE);
+            }
         } else {
             setVisibility(View.VISIBLE);
         }
         super.setText(text, type);
     }
-
 
     /**
      * 初始化view
@@ -70,16 +86,14 @@ public class YCRedDotView extends AppCompatTextView {
     }
 
     private void setLayoutParams() {
-        ViewGroup.LayoutParams layoutParams = getLayoutParams();
-        if(!(layoutParams instanceof LinearLayout.LayoutParams)){
-            LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(
+        if(!(getLayoutParams() instanceof LayoutParams)){
+            LayoutParams lParams = new LayoutParams(
                     ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT,
                     Gravity.END | Gravity.TOP);
             setLayoutParams(lParams);
         }
     }
-
 
     private void setTextView() {
         setTextColor(Color.WHITE);
@@ -90,9 +104,8 @@ public class YCRedDotView extends AppCompatTextView {
         setGravity(Gravity.CENTER);
     }
 
-
     private void setDefaultValues() {
-        setHideOnNull(true);
+        setHideNull(true);
         setBadgeCount(0);
     }
 
@@ -101,9 +114,10 @@ public class YCRedDotView extends AppCompatTextView {
      * @param dipRadius                 半径
      * @param badgeColor                颜色
      */
-    public void setBackground(int dipRadius, int badgeColor) {
+    private void setBackground(int dipRadius, int badgeColor) {
         int radius = dip2Px(dipRadius);
-        float[] radiusArray = new float[]{radius, radius, radius, radius, radius, radius, radius, radius};
+        float[] radiusArray = new float[]{radius, radius, radius, radius,
+                radius, radius, radius, radius};
         RoundRectShape roundRect = new RoundRectShape(radiusArray, null, null);
         ShapeDrawable bgDrawable = new ShapeDrawable(roundRect);
         bgDrawable.getPaint().setColor(badgeColor);
@@ -112,7 +126,7 @@ public class YCRedDotView extends AppCompatTextView {
         }
     }
 
-    public void setHideOnNull(boolean hideOnNull) {
+    public void setHideNull(boolean hideOnNull) {
         mHideOnNull = hideOnNull;
         setText(getText());
     }
@@ -126,6 +140,7 @@ public class YCRedDotView extends AppCompatTextView {
      * @param count                 数字
      */
     public void setBadgeCount(int count) {
+        //设置参数，超过99显示99+
         setBadgeCount(count > 99 ? "99+" : String.valueOf(count));
     }
 
@@ -133,6 +148,21 @@ public class YCRedDotView extends AppCompatTextView {
         setText(count);
     }
 
+    /**
+     * 设置小红点，不设置数字
+     */
+    public void setBadgeView(int dipRadius) {
+        setText("");
+        setWidth(dip2Px(dipRadius));
+        setHeight(dip2Px(dipRadius));
+        setBackground(9, Color.parseColor("#f14850"));
+    }
+
+    /**
+     * 设置支持TabWidget控件
+     * @param target                TabWidget
+     * @param tabIndex              索引
+     */
     public void setTargetView(TabWidget target, int tabIndex) {
         View tabView = target.getChildTabViewAt(tabIndex);
         setTargetView(tabView);
@@ -154,7 +184,7 @@ public class YCRedDotView extends AppCompatTextView {
 
     /**
      * 设置红点依附的view
-     * @param view
+     * @param view                  view
      */
     public void setTargetView(View view){
         if (getParent() != null) {
@@ -189,11 +219,20 @@ public class YCRedDotView extends AppCompatTextView {
      * @param gravity               位置
      */
     public void setRedHotViewGravity(int gravity){
-        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) getLayoutParams();
+        /*ViewGroup.LayoutParams layoutParams = getLayoutParams();
+        if (layoutParams instanceof FrameLayout.LayoutParams){
+            FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) getLayoutParams();
+            params.gravity = gravity;
+            setLayoutParams(params);
+        }else if(layoutParams instanceof LinearLayout.LayoutParams){
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) getLayoutParams();
+            params.gravity = gravity;
+            setLayoutParams(params);
+        }*/
+        LayoutParams params = (LayoutParams) getLayoutParams();
         params.gravity = gravity;
         setLayoutParams(params);
     }
-
 
     /**
      * 设置红点属性
@@ -203,9 +242,16 @@ public class YCRedDotView extends AppCompatTextView {
         setBadgeMargin(dipMargin, dipMargin, dipMargin, dipMargin);
     }
 
-
-    public void setBadgeMargin(int leftDipMargin, int topDipMargin, int rightDipMargin, int bottomDipMargin) {
-        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) getLayoutParams();
+    /**
+     * 设置红点的margin属性
+     * @param leftDipMargin         左边margin
+     * @param topDipMargin          上边margin
+     * @param rightDipMargin        右边margin
+     * @param bottomDipMargin       下边margin
+     */
+    public void setBadgeMargin(int leftDipMargin, int topDipMargin,
+                               int rightDipMargin, int bottomDipMargin) {
+        LayoutParams params = (LayoutParams) getLayoutParams();
         params.leftMargin = dip2Px(leftDipMargin);
         params.topMargin = dip2Px(topDipMargin);
         params.rightMargin = dip2Px(rightDipMargin);
@@ -214,7 +260,23 @@ public class YCRedDotView extends AppCompatTextView {
     }
 
 
-    public int dip2Px(float dip) {
+    /**
+     * 获取小红点的数量
+     * @return                      数量
+     */
+    public int getBadgeCount() {
+        if (getText() == null) {
+            return 0;
+        }
+        String text = getText().toString();
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    private int dip2Px(float dip) {
         return (int) (dip * getContext().getResources().getDisplayMetrics().density + 0.5f);
     }
 
--- app/src/main/java/com/ycbjie/ycreddotview/MainActivity.java
+++ app/src/main/java/com/ycbjie/ycreddotview/MainActivity.java
@@ -4,7 +4,14 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Spannable;
@@ -23,294 +30,177 @@ import android.text.style.TextAppearanceSpan;
 import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ycbjie.ycreddotviewlib.YCRedDotView;
 
-import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv2;
-    private TextView tv3;
-    private TextView tv4;
-    private TextView tv5;
-    private TextView tv6;
-    private TextView tv7;
-    private TextView tv8;
-    private TextView tv9;
-    private TextView tv10;
-    private TextView tv11;
-    private TextView tv12;
-    private TextView tv13;
 
+    private TabLayout tab_view;
+    private ViewPager view_pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String num1 = toFloat(10.00f);
-        String num2 = toFloat(10.01f);
-        String num3 = toFloat(10.10f);
-        Log.e("打印值-------",num1+"----"+num2+"-----"+num3);
-
         initView();
-        initSetView();
     }
 
     private void initView() {
         TextView tv1 = findViewById(R.id.tv_1);
-        tv2 = findViewById(R.id.tv_2);
-        tv3 = findViewById(R.id.tv_3);
-        tv4 = findViewById(R.id.tv_4);
-        tv5 = findViewById(R.id.tv_5);
-        tv6 = findViewById(R.id.tv_6);
-        tv7 = findViewById(R.id.tv_7);
-        tv8 = findViewById(R.id.tv_8);
-        tv9 = findViewById(R.id.tv_9);
-        tv10 = findViewById(R.id.tv_10);
-        tv11 = findViewById(R.id.tv_11);
-        tv12 = findViewById(R.id.tv_12);
-        tv13 = findViewById(R.id.tv_13);
+        TextView tv2 = findViewById(R.id.tv_2);
+        TextView tv3 = findViewById(R.id.tv_3);
+        LinearLayout ll_view_4 = findViewById(R.id.ll_view_4);
+        tab_view = findViewById(R.id.tab_view);
+        view_pager = findViewById(R.id.view_pager);
 
+        //创建红点View
         YCRedDotView ycRedDotView = new YCRedDotView(this);
+        //设置依附的View
         ycRedDotView.setTargetView(tv1);
+        //设置红点的数字
         ycRedDotView.setBadgeCount(10);
-        ycRedDotView.setRedHotViewGravity(Gravity.RIGHT);
+        //设置红点位置
+        ycRedDotView.setRedHotViewGravity(Gravity.END);
+        //获取小红点的数量
+        int count = ycRedDotView.getBadgeCount();
+        //如果是设置小红点，不设置数字，则可以用这个，设置属性是直径
+        //ycRedDotView.setBadgeView(10);
 
 
         YCRedDotView ycRedDotView2 = new YCRedDotView(this);
         ycRedDotView2.setTargetView(tv2);
         ycRedDotView2.setBadgeCount(1);
-        ycRedDotView2.setRedHotViewGravity(Gravity.RIGHT);
+        ycRedDotView2.setRedHotViewGravity(Gravity.START);
+
+        YCRedDotView ycRedDotView3 = new YCRedDotView(this);
+        ycRedDotView3.setTargetView(tv3);
+        ycRedDotView3.setBadgeView(10);
+        ycRedDotView3.setRedHotViewGravity(Gravity.END);
+        //设置margin
+        ycRedDotView3.setBadgeMargin(0,10,20,0);
+
+
+        YCRedDotView ycRedDotView4 = new YCRedDotView(this);
+        ycRedDotView4.setTargetView(ll_view_4);
+        ycRedDotView4.setBadgeCount(100);
+        ycRedDotView4.setHideNull(true);
+        int badgeCount = ycRedDotView4.getBadgeCount();
+        Log.d("小红点数量4---------",badgeCount+"");
+        ycRedDotView4.setRedHotViewGravity(Gravity.END);
+        ycRedDotView4.setBadgeMargin(0,10,20,0);
+
+        initFragmentList();
     }
 
 
-    private void initSetView() {
-        mode1();
-        mode2();
-        mode3();
-        mode4();
-        mode5();
-        mode6();
-        mode8();
-        mode9();
-        mode10();
-        mode11();
-        mode12();
-        mode13();
-    }
 
+    private void initFragmentList() {
+        ArrayList<String> mTitleList = new ArrayList<>();
+        ArrayList<Fragment> mFragments = new ArrayList<>();
+        mTitleList.add("日报");
+        mTitleList.add("主题");
+        mTitleList.add("专栏");
+        mTitleList.add("热门");
+        mFragments.add(new TabFragment());
+        mFragments.add(new TabFragment());
+        mFragments.add(new TabFragment());
+        mFragments.add(new TabFragment());
+        /*
+         * 这样setOffscreenPageLimit()就可以添加上，保留相邻2个实例，切换时不会卡
+         * 但会内存溢出，在显示时加载数据
+         */
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        ContentPageAdapter myAdapter = new ContentPageAdapter(supportFragmentManager,
+                mFragments, mTitleList,this);
+        view_pager.setAdapter(myAdapter);
+        myAdapter.notifyDataSetChanged();
+        // 左右预加载页面的个数
+        view_pager.setOffscreenPageLimit(mFragments.size());
+        tab_view.setTabMode(TabLayout.MODE_FIXED);
+        tab_view.setupWithViewPager(view_pager);
+        tab_view.setTabTextColors(getResources().getColor(R.color.colorAccent),
+                getResources().getColor(R.color.color_ffe935));
+        tab_view.setSelectedTabIndicatorHeight(2);
+        tab_view.setSelectedTabIndicatorColor(getResources().getColor(R.color.colorAccent));
+        tab_view.removeAllTabs();
 
-    public String toFloat(float num){
-        String backNum = "";
-        String strNum = String.valueOf(num);
-        Log.e("打印值fNum----","------------------"+strNum);
-        if(strNum.contains(".")){
-            String[] split = strNum.split("\\.");
-            String s1 = split[0];
-            String s2 = split[1];
-            int i = Integer.parseInt(s2);
-            if(i>0){
-                StringBuffer sb1 = new StringBuffer();
-                sb1.append(s1);
-                sb1.append(".");
-                sb1.append(s2);
-                float fNum = Float.parseFloat(sb1.toString());
-                //构造方法的字符格式这里如果小数不足2位,会以0补足.
-                DecimalFormat decimalFormat=new DecimalFormat(".00");
-                //format 返回的是字符串
-                backNum = decimalFormat.format(fNum);
-            }else {
-                StringBuffer sb2 = new StringBuffer();
-                sb2.append(s1);
-                //sb2.append(".");
-                //sb2.append("00");
-                backNum = sb2.toString();
-            }
+        for (int i = 0; i < mTitleList.size(); i++) {
+            TabLayout.Tab tab = tab_view.newTab();
+            tab.setCustomView(myAdapter.getTabView(i));
+            tab_view.addTab(tab);
         }
-        return backNum;
+
+
+        YCRedDotView ycRedDotView5 = new YCRedDotView(this);
+        ycRedDotView5.setTargetView(tab_view,0);
+        ycRedDotView5.setBadgeCount(100);
+        ycRedDotView5.setRedHotViewGravity(Gravity.END);
+        ycRedDotView5.setBadgeMargin(0,0,0,0);
+
+
+        YCRedDotView ycRedDotView = new YCRedDotView(this);
+        ycRedDotView.setTargetView(tab_view,1);
+        ycRedDotView.setBadgeCount(10);
+
+
+        YCRedDotView ycRedDotView6 = new YCRedDotView(this);
+        ycRedDotView6.setTargetView(tab_view,2);
+        ycRedDotView6.setBadgeView(10);
+        ycRedDotView6.setRedHotViewGravity(Gravity.END);
+        ycRedDotView6.setBadgeMargin(0,10,10,0);
     }
 
-    /**
-     * 使用SpannableString设置样式——字体颜色
-     */
-    private void mode1() {
-        SpannableString spannableString = new SpannableString("暗影IV已经开始暴走了");
-        ForegroundColorSpan colorSpan = new ForegroundColorSpan(Color.parseColor("#009ad6"));
-        spannableString.setSpan(colorSpan, 0, 8, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-        tv2.setText(spannableString);
-    }
+    public class ContentPageAdapter extends FragmentStatePagerAdapter {
 
-    /**
-     * 使用SpannableStringBuilder设置样式——字体颜色
-     */
-    private void mode2() {
-        SpannableStringBuilder spannableString = new SpannableStringBuilder();
-        spannableString.append("暗影IV");
-        spannableString.append("已经开始暴走了");
-        ForegroundColorSpan colorSpan = new ForegroundColorSpan(Color.parseColor("#009ad6"));
-        spannableString.setSpan(colorSpan, 0, 8, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-        tv3.setText(spannableString);
-    }
+        private ArrayList<Fragment> fragmentList;
+        private ArrayList<String> tabBeanList;
+        private FragmentActivity activity;
 
-    /**
-     * 使用SpannableStringBuilder设置样式——背景颜色
-     */
-    private void mode3() {
-        SpannableStringBuilder spannableString = new SpannableStringBuilder();
-        spannableString.append("暗影IV已经开始暴走了");
-        BackgroundColorSpan bgColorSpan = new BackgroundColorSpan(Color.parseColor("#009ad6"));
-        spannableString.setSpan(bgColorSpan, 0, 8, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-        tv4.setText(spannableString);
-    }
-
-    /**
-     * 使用SpannableStringBuilder设置样式——字体大小
-     */
-    private void mode4() {
-        SpannableStringBuilder spannableString = new SpannableStringBuilder();
-        spannableString.append("暗影IV已经开始暴走了");
-        AbsoluteSizeSpan absoluteSizeSpan = new AbsoluteSizeSpan(20);
-        spannableString.setSpan(absoluteSizeSpan, 0, 8, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-        tv5.setText(spannableString);
-    }
-    /**
-     * 使用SpannableStringBuilder设置样式——粗体\斜体
-     */
-    private void mode5() {
-        SpannableStringBuilder spannableString = new SpannableStringBuilder();
-        spannableString.append("暗影IV已经开始暴走了");
-        //setSpan可多次使用
-        StyleSpan styleSpan = new StyleSpan(Typeface.BOLD);//粗体
-        spannableString.setSpan(styleSpan, 0, 3, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-        StyleSpan styleSpan2 = new StyleSpan(Typeface.ITALIC);//斜体
-        spannableString.setSpan(styleSpan2, 3, 6, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-        StyleSpan styleSpan3 = new StyleSpan(Typeface.BOLD_ITALIC);//粗斜体
-        spannableString.setSpan(styleSpan3, 6, 9, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-        tv6.setText(spannableString);
-    }
-
-
-    /**
-     * 使用SpannableStringBuilder设置点击事件
-     */
-    private void mode6() {
-        SpannableStringBuilder spannableString = new SpannableStringBuilder();
-        spannableString.append("暗影IV已经开始暴走了");
-        ClickableSpan clickableSpan = new ClickableSpan() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "请不要点我", Toast.LENGTH_SHORT).show();
-            }
-        };
-        spannableString.setSpan(clickableSpan, 5, 8, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-        tv7.setText(spannableString);
-        tv7.setMovementMethod(LinkMovementMethod.getInstance());
-    }
-
-    /**
-     * 使用SpannableStringBuilder设置样式——图片
-     */
-    private void mode8() {
-        SpannableStringBuilder spannableString = new SpannableStringBuilder();
-        spannableString.append("暗影IV已经开始暴走了");
-        ImageSpan imageSpan = new ImageSpan(this, R.mipmap.ic_launcher);
-        //也可以这样
-        //Drawable drawable = getResources().getDrawable(R.mipmap.ic_launcher);
-        //drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
-        //ImageSpan imageSpan1 = new ImageSpan(drawable);
-        //将index为6、7的字符用图片替代
-        spannableString.setSpan(imageSpan, 6, 8, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-        tv8.setText(spannableString);
-    }
-
-    /**
-     * 使用SpannableStringBuilder设置样式——下划线
-     */
-    private void mode9() {
-        SpannableStringBuilder spannableString = new SpannableStringBuilder();
-        spannableString.append("暗影IV已经开始暴走了");
-        UnderlineSpan underlineSpan = new UnderlineSpan();
-        spannableString.setSpan(underlineSpan, 0, 8, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-        tv9.setText(spannableString);
-    }
-
-
-    private void mode10() {
-        String name = "name";
-        String title = "title";
-        SpannableString spannableString= new SpannableString(name+title);
-        spannableString.setSpan(new RoundBackgroundColorSpan(
-                Color.parseColor("#12DBD1"),Color.parseColor("#FFFFFF")),
-                0, name.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        tv10.setText(spannableString);
-    }
-
-
-    private void mode11() {
-        String[] tags = {"精华", "活动","推荐"};
-
-        SpannableStringBuilder stringBuilder = new SpannableStringBuilder();
-        for (String tag : tags) {
-            String thisTag = " " + tag + " ";
-            stringBuilder.append(thisTag);
-            RoundedBackgroundSpan span;
-            if("活动".equals(tag)){
-                span= new RoundedBackgroundSpan(ContextCompat.getColor(this,R.color.colorAccent), Color.WHITE);
-            }else if ("推荐".equals(tag)){
-                span= new RoundedBackgroundSpan(ContextCompat.getColor(this,R.color.colorPrimary), Color.WHITE);
-            }else{
-                span= new RoundedBackgroundSpan(ContextCompat.getColor(this,R.color.colorPrimaryDark), Color.WHITE);
-            }
-            stringBuilder.setSpan(span, stringBuilder.length() - thisTag.length(), stringBuilder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            stringBuilder.append(" ");
+        ContentPageAdapter(FragmentManager fm, ArrayList<Fragment> fragmentList,
+                           ArrayList<String> tabBeanList, FragmentActivity mainActivity) {
+            super(fm);
+            this.activity = mainActivity;
+            this.fragmentList = fragmentList;
+            this.tabBeanList = tabBeanList;
         }
-        stringBuilder.append("王宝强凌晨宣布离婚，妻子劈腿经纪人。");
-        tv11.setText(stringBuilder);
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList==null ? 0 : fragmentList.size();
+        }
+
+
+        View getTabView(int position) {
+            View view = LayoutInflater.from(activity).inflate(R.layout.main_tab_layout,
+                    null, false);
+            TextView textView = view.findViewById(R.id.tv_tab_text);
+            textView.setText(tabBeanList.get(position));
+            return view;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+
+        }
     }
 
-    private void mode12() {
-        SpannableStringBuilder string = SpannableUtils.appendString(
-                this, "名字", "放假啊打扫房间");
-        tv12.setText(string);
-    }
-
-
-    private void mode13() {
-        SpannableStringBuilder string = appendString(
-                this, "名字", "放假啊打扫房间放假啊打扫房间放假啊打扫房间");
-        tv13.setText(string);
-    }
-
-
-
-    /**
-     * 字符串拼接
-     * @param context                   上下文
-     * @param beforeText                前面标签文件
-     * @param afterText                 后面内容
-     * @return
-     */
-    public SpannableStringBuilder appendString(
-            Context context , String beforeText , String afterText){
-        SpannableStringBuilder stringBuilder = new SpannableStringBuilder();
-        String thisTag = " " + beforeText + " ";
-        stringBuilder.append(thisTag);
-        stringBuilder.append("  ");
-        stringBuilder.append(afterText);
-        RoundedBackgroundSpan span;
-
-        RelativeSizeSpan sizeSpan = new RelativeSizeSpan(0.8f);
-        stringBuilder.setSpan(sizeSpan,0, thisTag.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        span= new RoundedBackgroundSpan(ContextCompat.getColor(context,
-                R.color.color_ffe935), R.color.colorAccent);
-        stringBuilder.setSpan(span, 0, thisTag.length(), Spanned.SPAN_USER_SHIFT);
-
-        stringBuilder.append(" ");
-        return stringBuilder;
-    }
 
 }
